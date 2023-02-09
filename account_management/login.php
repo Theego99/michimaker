1b1c2648d23e28e36cc13025579830e117bf81e8
Diego Alonso <dalca993@gmail.com>
2023-02-09 18:12:49 +0900

    login page complete

--- account_management/login.php
+++ account_management/login.php
@@ -33,33 +33,39 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 
 ?>
-<!DOCTYPE html>
-<html>
+
 <head>
-    <title>Login</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../style.css">
+
+  <title>michi maker hp</title>
+  <!--ファビコンの設定-->
+<link rel="shortcut icon" href="./images/favicon.ico">
+<link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png">
 </head>
+
 <body>
-    
-    <h1>ログイン</h1>
-    
-    <?php if ($is_invalid): ?>
+
+  <div class ="login-header">
+    <img class ="login-image" src="../design/images/NUKEMICHI_login.png">
+</div>
+
+      <h2 class ="new-user-btn"><a href="./signup.html">新規登録はこちら</a></h2>
+
+  <?php if ($is_invalid): ?>
         <em>メールアドレスまたはパスワードが間違っています</em>
     <?php endif; ?>
     
-    <form method="post">
-        <label for="email">メールアドレス</label>
+    <form method="post" id="login-form">
         <input type="email" name="email" id="email"
-               value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+               value="<?= htmlspecialchars($_POST["email"] ?? "") ?>" placeholder="メールアドレス" required>
         
-        <label for="password">パスワード</label>
-        <input type="password" name="password" id="password">
+        <input type="password" name="password" id="password" placeholder="パスワード" required>
         
-        <button>送信</button>
+        <input id="login-submitbtn" type="submit" value="ログイン">
     </form>
-<a href="./signup.html">
-    <p>アカウントを持っていない </p>
-</a>
 </body>
+
 </html>
