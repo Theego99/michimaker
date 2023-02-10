5bf735fbf68b20ef250a4953ed595674a01d724c
Diego Alonso <dalca993@gmail.com>
2023-02-10 17:14:56 +0900

    search update

--- search.php
+++ search.php
@@ -2,14 +2,11 @@
 $mysqli = require __DIR__ . "/account_management/database.php";
 
 // (C) SEARCH
-$stmt = $mysqli->prepare("select * from shortcuts where name = ? or comments = ?;");
-$stmt->bind_param(
-    "ss",
-    $$_POST["search"],
-    $$_POST["search"]
-);
-$stmt->execute();
-$results = $stmt->fetchAll();
+$stmt = $mysqli->prepare("select * from shortcuts where name like ? or comments like ?;");
+
+$stmt->execute(["%".$_POST["search"]."%", "%".$_POST["search"]."%"]);
+$resultSet = $stmt->get_result();
+$results = $resultSet->fetch_All(MYSQLI_ASSOC);
 if (isset($_POST["ajax"])) {
     echo json_encode($results);
 }
