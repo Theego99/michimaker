349f4e15d0fd5364929d935224f92b518b1e2333
Diego Alonso <dalca993@gmail.com>
2023-02-11 20:08:21 +0900

    update

--- account_management/login.php
+++ account_management/login.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
             $_SESSION["user_id"] = $user["id"];
 
-            header("Location: index.php");
+            header("Location: ../mypage.php");
             exit;
         }
     }
