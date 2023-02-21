fb335954cb5d56a5ad1a68f90cc0b864e2d0bda8
Theego99 <56567931+Theego99@users.noreply.github.com>
2023-02-21 15:31:47 +0900

    Add files via upload

--- search.php
+++ search.php
@@ -3,8 +3,9 @@ $mysqli = require __DIR__ . "/account_management/database.php";
 
 // 検索
 $stmt = $mysqli->prepare("select * from shortcuts where name like ? or comments like ? or address like ?;");
+$stmt = $mysqli->prepare("SELECT s.*, u.name FROM shortcuts s JOIN users u ON s.user_id = u.id WHERE s.name LIKE ? OR s.comments LIKE ? OR s.address LIKE ? OR u.name LIKE ?;");
 
-$stmt->execute(["%".$_POST["search"]."%", "%".$_POST["search"]."%", "%".$_POST["search"]."%"]);
+$stmt->execute(["%".$_POST["search"]."%", "%".$_POST["search"]."%", "%".$_POST["search"]."%", "%".$_POST["search"]."%"]);
 $resultSet = $stmt->get_result();
 $results = $resultSet->fetch_All(MYSQLI_ASSOC);
 if (isset($_POST["ajax"])) {
